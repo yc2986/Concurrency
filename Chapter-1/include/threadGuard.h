71e7473638e652a9a9009c900c1d8713d5b0af1a
yc2986 <yc2986@columbia.edu>
2016-09-01 07:22:40 -0700

    modify

--- Chapter-1/include/threadGuard.h
+++ Chapter-1/include/threadGuard.h
@@ -7,8 +7,8 @@ class thread_guard {
 	std::thread &t;
 public:
 	explicit thread_guard(std::thread &t_): t(t_) {}
-	thread_guard(thread_guard const&) = delete;		// disable copy constructor
-	thread_guard& operator=(thread_guard const&) = delete;	// disable = operator
+	thread_guard(const thread_guard &) = delete;		// disable copy constructor
+	thread_guard& operator=(const thread_guard &) = delete;	// disable = operator
 	~thread_guard();
 };
 
